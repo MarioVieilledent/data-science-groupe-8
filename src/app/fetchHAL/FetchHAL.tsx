bfa149ccba46c19a8f4eacfa794c738c6af2156c
Mario Vieilledent <mario.vieilledent@gmail.com>
2022-05-11 14:18:01 +0200

    Début reprise api hal

--- src/app/fetchHAL/FetchHAL.tsx
+++ src/app/fetchHAL/FetchHAL.tsx
@@ -4,7 +4,7 @@ import localStorageService from '../localStoageService';
 import './FetchHAL.scss';
 
 const entryPoints = {
-  root: 'http://api.archives-ouvertes.fr/search/',
+  root: 'http://api.archives-ouvertes.fr/search',
   portailInstances: 'https://api.archives-ouvertes.fr/ref/instance'
 }
 
@@ -18,6 +18,7 @@ class FetchHAL extends React.Component<Props, State>{
     // Création de l'objet state de type State
     this.state = {
       portailInstancesList: [],
+      scientificPublicationList: [],
       apiMaker: {
         q: '*',
         wt: 'json',
@@ -59,6 +60,30 @@ class FetchHAL extends React.Component<Props, State>{
   /**
    * Compile les éléments de state.apiMaker pour créer l'URL
    */
+  buildUrl(): string {
+    let url = entryPoints.root + '?';
+    const list = []
+    for (const [key, value] of Object.entries(this.state.apiMaker)) {
+      list.push(key + '=' + value);
+    }
+    url += list.join('&');
+    return url;
+  }
+
+  /**
+   * Constuit l'url et effectue une recherche ur l'API de HAL
+   */
+  fetchHAL(): void {
+    let scientificPublicationList: JSX.Element[] = [];
+    const url = this.buildUrl();
+
+    httpService(url, (data: any) => {
+      console.log(data.response.docs)
+      scientificPublicationList = this.buildScientificPublicationList(data.response.docs);
+    });
+
+    this.setState({ scientificPublicationList: scientificPublicationList });
+  }
 
   /**
    * Construit la liste JSX.Element des instances de portail
@@ -75,17 +100,44 @@ class FetchHAL extends React.Component<Props, State>{
     });
   }
 
+  /**
+   * Construit la liste JSX.Element des instances de portail
+   */
+  buildScientificPublicationList(list: ScientificPublication[]) {
+    return list.map((doc: ScientificPublication) => {
+      return (
+        <div className="doc-elem flex" key={doc.docid}>
+          <button title="Ouvrir document" className="code">{doc.label_s}</button>
+          <a className="url" href={doc.uri_s}>Link</a>
+        </div>
+      )
+    });
+  }
+
   render() {
     return (
       <div className="container flex-col">
         <a href="https://api.archives-ouvertes.fr/docs/search" target="_blank" rel="noreferrer">API HAL</a>
         <div className="search-div">
-          <input className="search-input" type="text" onChange={(e: any) => { console.log(e.target.value) }} />
+          <input className="search-input" type="text" onChange={(e: any) => {
+            this.setState((prev: any) => {
+              const apiMaker = Object.assign({}, prev.apiMaker);
+              apiMaker.q = e.target.value;
+              return { apiMaker };
+            })
+          }} />
+          <button onClick={() => { this.fetchHAL(); }}>Rechercher</button>
         </div>
         <div className="block flex-col">
           <span className="title-description">Instances de portail</span>
           <div className="portail-instances-list flex-col">
             {this.state.portailInstancesList}
+          </div>
+        </div>
+        <div className="block flex-col">
+          <span className="title-description">Résultat de recherche</span>
+          <div className="scientific-publication-list flex-col">
+            {this.state.scientificPublicationList}
           </div>
         </div>
       </div>
@@ -100,9 +152,16 @@ interface Doc {
   url: string;
 }
 
+interface ScientificPublication {
+  docid: string;
+  label_s: string;
+  uri_s: string;
+}
+
 // eslint-disable-next-line @typescript-eslint/no-empty-interface
 interface State {
   portailInstancesList: JSX.Element[];
+  scientificPublicationList: JSX.Element[];
   apiMaker: any;
 }
 
--- src/app/http-service.ts
+++ src/app/http-service.ts
@@ -1,6 +1,7 @@
 
 // eslint-disable-next-line @typescript-eslint/ban-types
 function httpService(url: string, func: Function): void {
+    console.warn(url);
     const headers = new Headers();
     headers.append('Content-Type', 'text/json');
     fetch(url, { method: 'GET', mode: 'cors' }).then(res => {
